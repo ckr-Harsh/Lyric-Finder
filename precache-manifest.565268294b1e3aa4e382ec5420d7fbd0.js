b7dd6bb82e56a1683b8e1e5f672b8b22986924c7
ckr-Harsh <xharsh790@gmail.com>
2020-12-01 15:22:47 +0530

    Updates

---
+++ precache-manifest.565268294b1e3aa4e382ec5420d7fbd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d622b1185d0e75e6f009cea41ca7a35",
+    "url": "/Lyric-Finder/index.html"
+  },
+  {
+    "revision": "4bd573ab15e5a566130d",
+    "url": "/Lyric-Finder/static/css/main.54b6b345.chunk.css"
+  },
+  {
+    "revision": "97e93141763920262758",
+    "url": "/Lyric-Finder/static/js/2.b8454ef8.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Lyric-Finder/static/js/2.b8454ef8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bd573ab15e5a566130d",
+    "url": "/Lyric-Finder/static/js/main.29f23e4a.chunk.js"
+  },
+  {
+    "revision": "d0719966fe31f916544b",
+    "url": "/Lyric-Finder/static/js/runtime-main.0a98c90c.js"
+  },
+  {
+    "revision": "894673b4d45bb5ca7a0d91685c407d90",
+    "url": "/Lyric-Finder/static/media/triangle.894673b4.gif"
+  }
+]);
