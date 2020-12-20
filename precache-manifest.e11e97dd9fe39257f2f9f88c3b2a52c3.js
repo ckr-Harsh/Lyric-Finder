43f2908df33d61278f4455c88ceede9e70a0ab36
ckr-Harsh <xharsh790@gmail.com>
2020-12-20 09:27:07 +0530

    Updates

---
+++ precache-manifest.e11e97dd9fe39257f2f9f88c3b2a52c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d3f55e86a47c4b1b248d0f799765df",
+    "url": "/Lyric-Finder/index.html"
+  },
+  {
+    "revision": "98cecf09815fef3fe6b7",
+    "url": "/Lyric-Finder/static/css/main.ff147a29.chunk.css"
+  },
+  {
+    "revision": "21b75c645bd89683c163",
+    "url": "/Lyric-Finder/static/js/2.27b01c13.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Lyric-Finder/static/js/2.27b01c13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98cecf09815fef3fe6b7",
+    "url": "/Lyric-Finder/static/js/main.570ba763.chunk.js"
+  },
+  {
+    "revision": "d0719966fe31f916544b",
+    "url": "/Lyric-Finder/static/js/runtime-main.0a98c90c.js"
+  },
+  {
+    "revision": "894673b4d45bb5ca7a0d91685c407d90",
+    "url": "/Lyric-Finder/static/media/triangle.894673b4.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5c6f75b0373e3ade5930daec0ba8e25b.js"
+  "/Lyric-Finder/precache-manifest.e11e97dd9fe39257f2f9f88c3b2a52c3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Lyric-Finder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
